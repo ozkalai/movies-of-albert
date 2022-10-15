45514494dd09d18b8d625756a68bd546140f4b6b
ozkalai <melihozkalayy@gmail.com>
2022-10-15 13:50:28 +0300

    feat: chakra with custom theme and content provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ContentByLocaleProvider } from "../src/hooks/useContentByLocale";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  background: {
+    100: "#212a32",
+  },
+  header: {
+    100: "#f26347",
+    200: "#f21301",
+  },
+  sider: {
+    100: "#181c1f",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+console.log("theme", theme);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <ContentByLocaleProvider>
+        <Component {...pageProps} />
+      </ContentByLocaleProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
