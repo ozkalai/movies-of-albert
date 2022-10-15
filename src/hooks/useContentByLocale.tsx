aaff3d338529f8a7abe6b87fb7f3acb262a4b401
ozkalai <melihozkalayy@gmail.com>
2022-10-15 13:49:52 +0300

    feat: content by locale custom hook

---
+++ src/hooks/useContentByLocale.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+interface Categories {
+  popular: string;
+  topRated: string;
+  upcoming: string;
+}
+
+interface Content {
+  categories: Categories;
+}
+
+interface Translations {
+  en: Content;
+  tr: Content;
+}
+
+const translations: Translations = {
+  en: {
+    categories: {
+      popular: "Popular Movies",
+      topRated: "Top Rated Movies",
+      upcoming: "Upcoming Movies",
+    },
+  },
+  tr: {
+    categories: {
+      popular: "Popüler Filmler",
+      topRated: "En İyi Filmler",
+      upcoming: "Yaklaşan Filmler",
+    },
+  },
+};
+
+const ContentByLocaleContext = React.createContext<Content>({} as Content);
+
+export const ContentByLocaleProvider = ({ children }: any) => {
+  const { locale } = useRouter();
+  const content = translations[(locale || "en") as keyof Translations];
+
+  return (
+    <ContentByLocaleContext.Provider value={content}>
+      {children}
+    </ContentByLocaleContext.Provider>
+  );
+};
+
+export const useContentByLocale = () => {
+  const context = React.useContext(ContentByLocaleContext);
+
+  return context;
+};
