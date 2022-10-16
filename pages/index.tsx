713b88acf1e54eada8156d567bf3790d58a3ca4c
ozkalai <melihozkalayy@gmail.com>
2022-10-16 17:01:41 +0300

    feat: initial main page with static props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,54 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import type { GetStaticProps, NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
-const Home: NextPage = () => {
+import styles from "../styles/Home.module.css";
+import { getMovies } from "../src/services/get-movies";
+import { Languages } from "../src/typings/translation";
+import { IMovie } from "../src/typings/movie";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+interface IHomeProps {
+  popularMovies: IMovie[];
+  topRatedMovies: IMovie[];
+  upcomingMovies: IMovie[];
+}
+
+export const getStaticProps: GetStaticProps<IHomeProps> = async ({
+  locale,
+}) => {
+  const popularMovies = await getMovies(
+    (locale || "en-US") as Languages,
+    "popular"
+  );
+  const topRatedMovies = await getMovies(
+    (locale || "en-US") as Languages,
+    "top_rated"
+  );
+  const upcomingMovies = await getMovies(
+    (locale || "en-US") as Languages,
+    "upcoming"
+  );
+
+  return {
+    props: {
+      popularMovies,
+      topRatedMovies,
+      upcomingMovies,
+    },
+  };
+};
+
+const Home: NextPage<IHomeProps> = ({
+  popularMovies,
+  topRatedMovies,
+  upcomingMovies,
+}) => {
+  const [movies, setMovies] = React.useState<IMovie[]>(popularMovies);
+
+  const { locale } = useRouter();
   return (
     <div className={styles.container}>
       <Head>
@@ -13,60 +58,20 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <Link
+              href={`/movie/${movie.id}`}
+              as={`/movie/${movie.id}`}
+              locale={locale}
+            >
+              <a>{movie.title}</a>
+            </Link>
+          </li>
+        ))}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
