af0e58b37ca753150ec8c1a8737d362441723cd4
ozkalai <melihozkalayy@gmail.com>
2022-10-18 18:41:35 +0300

    fix: detail page

--- pages/movie/[id].tsx
+++ pages/movie/[id].tsx
@@ -1,11 +1,14 @@
 import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import Image from "next/image";
+import React from "react";
 import { useRouter } from "next/router";
+import { Box, IconButton, Flex, Text } from "@chakra-ui/react";
+import { ArrowBackIcon } from "@chakra-ui/icons";
 
 import { getMovie } from "../../src/services/get-movie";
 import { IMovieDetail } from "../../src/typings/movie";
 import { Languages } from "../../src/typings/translation";
+import Layout from "../../src/components/Layout";
 
 interface IDetailProps {
   movie: IMovieDetail;
@@ -14,8 +17,6 @@ interface IDetailProps {
 export const getStaticProps: GetStaticProps<IDetailProps> = async (context) => {
   const { id } = context.params as { id: string };
   const movie = await getMovie((context.locale as Languages) || "en-US", id);
-
-  console.log("movie", movie);
 
   if (!movie) {
     return {
@@ -42,28 +43,62 @@ const Detail: NextPage<IDetailProps> = ({ movie }) => {
   const { back } = useRouter();
 
   return (
-    <div>
-      <button
-        onClick={back}
-        className="mb-12 bg-indigo-500 px-4 py-2 text-white "
-      >
-        Go back
-      </button>
-      <div>
-        <h1 className="text-2xl font-bold text-black">{movie.title}</h1>
-        <div className="flex py-2 gap-2.5">
-          {movie.genres.map((genre) => (
-            <div
-              key={genre.id}
-              className="bg-black text-white rounded-2xl px-2.5 py-1"
-            >
-              {genre.name}
-            </div>
-          ))}
-        </div>
-        <p>{movie.overview}</p>
-      </div>
-    </div>
+    <Layout>
+      <Box color="white" p={{ base: "2", md: "10" }}>
+        <IconButton
+          _hover={{ bg: "header.200" }}
+          icon={<ArrowBackIcon />}
+          bg="transparent"
+          aria-label="back-button"
+          onClick={back}
+          mb={{ base: "2", md: "10" }}
+        >
+          Back
+        </IconButton>
+        <Flex
+          display="flex"
+          justifyContent={{ base: "center", md: "flex-start" }}
+          alignItems={{ base: "center", md: "flex-start" }}
+          flexDirection={{ base: "column", md: "row" }}
+        >
+          <div
+            style={{
+              width: "200px",
+              height: "300px",
+              position: "relative",
+            }}
+          >
+            <Image
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+              layout="fill"
+            />
+          </div>
+
+          <Flex
+            px={10}
+            flex="2"
+            direction="column"
+            gap="10px"
+            mt={{ base: 4, md: 0 }}
+          >
+            <Text fontWeight="semibold" fontSize={"2xl"}>
+              {movie.title}
+            </Text>
+            <Text>{movie.overview}</Text>
+            <Text>{`Duration: ${movie.runtime} min`}</Text>
+            <Flex>
+              Genre:
+              {movie.genres.map((genre) => (
+                <Text key={genre.id} ml={1}>
+                  {genre.name}
+                </Text>
+              ))}
+            </Flex>
+          </Flex>
+        </Flex>
+      </Box>
+    </Layout>
   );
 };
 
